2b789631fd813b90de63348ec0e7a27cc1fd1e20
0xDefy <suryareddy792@gmail.com>
2025-03-20 20:32:05 +0530

    added database interactions and changed routes /api/reset /api/users /api/chirps

--- main.go
+++ main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
+	// "context"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"regexp"
 	"strings"
 	"sync/atomic"
+	"time"
+
+	"github.com/0xdefy/chirpy/internal/database"
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileServerHits atomic.Int32
+	DB             *database.Queries
+	PLATFORM       string
+}
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
 }
 
 // All helper functions
@@ -75,7 +93,18 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 }
 
 func main() {
-	apiCfg := apiConfig{}
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	platform := os.Getenv("PLATFORM")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("error connecting to database: %v", err)
+	}
+	dbQueries := database.New(db)
+	apiCfg := apiConfig{
+		DB:       dbQueries,
+		PLATFORM: platform,
+	}
 	serveMux := http.NewServeMux()
 	serveMux.Handle("/app/",
 		apiCfg.middlewareMetricsInc(
@@ -84,13 +113,14 @@ func main() {
 					http.Dir(".")))))
 	serveMux.HandleFunc("GET /api/healthz", healthzHandle)
 	serveMux.HandleFunc("GET /admin/metrics", apiCfg.metricsHandler)
-	serveMux.HandleFunc("POST /admin/reset", apiCfg.resetMetricsHandler)
-	serveMux.HandleFunc("POST /api/validate_chirp", apiCfg.validateChirpHandler)
+	serveMux.HandleFunc("POST /api/users", apiCfg.createUserHandler)
+	serveMux.HandleFunc("POST /admin/reset", apiCfg.resetHandler)
+	serveMux.HandleFunc("POST /api/chirps", apiCfg.chirpsHandler)
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: serveMux,
 	}
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	if err != nil {
 		log.Fatalf("error listening and serving: %v", err)
 	}
@@ -124,16 +154,26 @@ func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(y))
 }
 
-func (cfg *apiConfig) resetMetricsHandler(w http.ResponseWriter, r *http.Request) {
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	if cfg.PLATFORM != "dev" {
+		respondWithError(w, 403, "Forbidden")
+		return
+	}
+	err := cfg.DB.DeleteUsers(r.Context())
+	if err != nil {
+		log.Printf("error while deleting users %v", err)
+		respondWithError(w, http.StatusInternalServerError, "Failed to reset users")
+		return
+	}
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(200)
-	cfg.fileServerHits.Swap(0)
-	w.Write([]byte("Hits: 0"))
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Successfully deleted all users"))
 }
 
-func (cfg *apiConfig) validateChirpHandler(w http.ResponseWriter, r *http.Request) {
+func (cfg *apiConfig) chirpsHandler(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
-		Body string `json:"body"`
+		Body   string    `json:"body"`
+		UserId uuid.UUID `json:"user_id"`
 	}
 	decoder := json.NewDecoder(r.Body)
 	params := parameters{}
@@ -149,12 +189,59 @@ func (cfg *apiConfig) validateChirpHandler(w http.ResponseWriter, r *http.Reques
 		respondWithError(w, 400, "Chirp is too long")
 		return
 	}
-	type successResponse struct {
-		CleanedBody string `json:"cleaned_body"`
+	dbParams := database.CreateChirpParams{
+		Body:   params.Body,
+		UserID: params.UserId,
 	}
-	cleanedString := cleanText(params.Body)
-	respBody := successResponse{
-		CleanedBody: cleanedString,
+	dbChirps, err := cfg.DB.CreateChirp(r.Context(), dbParams)
+	if err != nil {
+		log.Printf("error uploading to database: %s", err)
+		respondWithError(w, 500, "uploading to database failed")
+		return
 	}
-	respondWithJSON(w, 200, respBody)
+	type chirpResponse struct {
+		ID        uuid.UUID `json:"id"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+		Body      string    `json:"body"`
+		UserID    uuid.UUID `json:"user_id"`
+	}
+
+	// Then convert dbChirps to this response format
+	response := chirpResponse{
+		ID:        dbChirps.ID,
+		CreatedAt: dbChirps.CreatedAt,
+		UpdatedAt: dbChirps.UpdatedAt,
+		Body:      dbChirps.Body,
+		UserID:    dbChirps.UserID,
+	}
+	respondWithJSON(w, 201, response)
+}
+
+func (cfg *apiConfig) createUserHandler(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Email string `json:"email"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("error decoding parameters: %s", err)
+		respondWithError(w, 500, "Something went wrong")
+		return
+	}
+	params.Email = strings.TrimSpace(params.Email)
+	dbUser, err := cfg.DB.CreateUser(r.Context(), params.Email)
+	if err != nil {
+		log.Printf("error uploading to database: %s", err)
+		respondWithError(w, 500, "uploading to database failed")
+		return
+	}
+	user := User{
+		ID:        dbUser.ID,
+		CreatedAt: dbUser.CreatedAt,
+		UpdatedAt: dbUser.UpdatedAt,
+		Email:     dbUser.Email,
+	}
+	respondWithJSON(w, 201, user)
 }
